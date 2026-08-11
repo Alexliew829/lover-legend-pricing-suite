c10c1cf514c1d88014c693d1571fd72e60d4e1f1
Alexliew829 <alexliew2026@gmail.com>
2026-08-12 04:04:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lover-legend-cost-pricing-vV6.0-fixed-import-mapping";
+const CACHE = "lover-legend-cost-pricing-vV6.1-fixed-import-mapping";
 const CORE = [
   "./",
   "./index.html",
