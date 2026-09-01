02e71cee54e45713c9dc0603d4f3f1f3da10b238
Alexliew829 <alexliew2026@gmail.com>
2026-09-01 21:39:14 +0800

    Add files via upload

--- bonsai-price-calculator/sw.js
+++ bonsai-price-calculator/sw.js
@@ -1,22 +1,19 @@
-// V3.3 legacy service-worker cleanup.
-// This app no longer uses offline page caching so Home Screen always loads the latest deployment.
 self.addEventListener("install", function () {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function (event) {
   event.waitUntil(
-    Promise.all([
-      caches.keys().then(function (keys) {
-        return Promise.all(keys.map(function (key) {
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
           return caches.delete(key);
-        }));
-      }),
-      self.registration.unregister()
-    ]).then(function () {
-      return self.clients.claim();
+        })
+      );
     })
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
