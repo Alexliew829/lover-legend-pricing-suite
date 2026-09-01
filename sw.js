a33055eb7a49d1e83eb9bd4f625db426f6ee4e6f
Alexliew829 <alexliew2026@gmail.com>
2026-09-01 23:33:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = "lover-legend-pricing-v8.7-smooth-v33";
+const CACHE = "lover-legend-pricing-v8.8-native-v33";
 const CORE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./cost-calculator/index.html?v=8.7-smooth-scroll-v33",
-  "./bonsai-price-calculator/index.html?v=3.3-v8.7"
+  "./cost-calculator/index.html?v=8.8-native-scroll-v33",
+  "./bonsai-price-calculator/index.html?v=3.3-v8.8"
 ];
 
 self.addEventListener("install", event => {
