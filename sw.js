87259dc85c1f91ffd0d634256ad3d872e5fdf8d7
Alexliew829 <alexliew2026@gmail.com>
2026-09-10 00:01:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Lover Legend Pricing Suite V9.9 - legacy service worker cleanup
+// Lover Legend Pricing Suite V10.0 - legacy service worker cleanup
 self.addEventListener("install", function () {
   self.skipWaiting();
 });
